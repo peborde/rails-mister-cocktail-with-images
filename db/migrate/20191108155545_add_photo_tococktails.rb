afe8c20e2a46ed6fc04516d93797b8faa9023012
pierre etienne bordenave <pierre.etienne.bordenave@gmail.com>
2019-11-08 17:05:42 +0100

    images upload with cloudinary

---
+++ db/migrate/20191108155545_add_photo_tococktails.rb
@@ -0,0 +1,4 @@
+class AddPhotoTococktails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ data = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
 
 JSON.parse(data)['drinks'].each do |drink|
   Ingredient.create!(name: drink['strIngredient1'])
-end
 
+url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+article = Article.new(title: 'NES', body: "A great console")
+article.remote_photo_url = url
+article.save
+end
 
