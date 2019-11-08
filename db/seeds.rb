8a5ffc93fdacf099185eab2813c5ba10782af3a7
pierre etienne bordenave <pierre.etienne.bordenave@gmail.com>
2019-11-08 14:37:27 +0100

    add link on each path

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@ data = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
 JSON.parse(data)['drinks'].each do |drink|
   Ingredient.create!(name: drink['strIngredient1'])
 end
+
+
